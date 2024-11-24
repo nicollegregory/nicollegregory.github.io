fb9764aad191ce5b54ea5421adb0a8be925636e4
nicollegregory <gregn565@newschool.edu>
2024-11-23 21:45:15 -0500

    debugging

--- entry-8/script.js
+++ entry-8/script.js
@@ -9,7 +9,7 @@ let choice = document.getElementById("answer").value.trim().toLowerCase();
 
 let imageElement = document.getElementById("my-image");
 
-
+console.log("User's Guess: ", choice);
 if (choice === "weezer") {
    imageElement.src = "https://i.redd.it/59a086j8yz971.jpg";
 }
@@ -26,7 +26,7 @@ function beatles() {
   let choice = document.getElementById("answer2").value.trim().toLowerCase();
 
   let imageElement = document.getElementById("image");
-
+  console.log("User's Guess: ", choice);
   if (choice === "the beatles") {
       imageElement.src = "https://preview.redd.it/john-lemon-v0-13lgm085bs591.jpg?auto=webp&s=01d8a311f3dd3b36481599315908703cefa41312"; 
   } else {
@@ -41,7 +41,7 @@ function wheatus() {
   let choice = document.getElementById("answer3").value.trim().toLowerCase();
 
   let imageElement = document.getElementById("teen");
-
+  console.log("User's Guess: ", choice);
   if (choice === "wheatus") {
       imageElement.src = "https://static.spin.com/files/2017/11/Screen-Shot-2017-11-03-at-4.25.58-PM-1509743654.png"; 
   } else {
@@ -57,7 +57,7 @@ function michael() {
   let choice = document.getElementById("answer4").value.trim().toLowerCase();
 
   let imageElement = document.getElementById("beat");
-
+  console.log("User's Guess: ", choice);
   if (choice === "michael jackson") {
       imageElement.src = "https://i.pinimg.com/originals/78/db/61/78db61a46989954df4bbdf9097280521.jpg"; 
   } else {
@@ -72,7 +72,7 @@ function television() {
   let choice = document.getElementById("answer5").value.trim().toLowerCase();
 
   let imageElement = document.getElementById("moon");
-
+  console.log("User's Guess: ", choice);
   if (choice === "television") {
       imageElement.src = "https://cdn.dribbble.com/users/206784/screenshots/2558004/media/5d6bea89977e20484819200b17ae0175.jpg?resize=400x300&vertical=center"; 
   } else {
