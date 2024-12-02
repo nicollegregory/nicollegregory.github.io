fca6d8bf4deb102961fa4482ba5aba3b59658ae3
nicollegregory <gregn565@newschool.edu>
2024-12-02 10:46:21 -0500

    my melody meme

--- entry-3/script.js
+++ entry-3/script.js
@@ -16,7 +16,7 @@ function xcx() {
   if (choice === "charli xcx") {
       imageElement.src = "https://jenesaispop.com/wp-content/uploads/2024/06/Captura-de-pantalla-2024-06-21-195255.png";
   } else {
-      imageElement.src = "https://th.bing.com/th/id/OIP.OH8xtUjQwnJBeCero53anQAAAA?rs=1&pid=ImgDetMain";
+      imageElement.src = "https://i.pinimg.com/474x/5a/e9/0e/5ae90e60e56dc3965da6d1d398ecbca0.jpg";
   }
 
 
@@ -43,7 +43,7 @@ function martinez() {
   if (choice === "melanie martinez") {
       imageElement.src = "https://images6.fanpop.com/image/photos/38800000/melanie-martinez-melanie-martinez-38849836-540-304.png";
   } else {
-      imageElement.src = "https://th.bing.com/th/id/OIP.OH8xtUjQwnJBeCero53anQAAAA?rs=1&pid=ImgDetMain";
+      imageElement.src = "https://i.pinimg.com/474x/5a/e9/0e/5ae90e60e56dc3965da6d1d398ecbca0.jpg";
   }
 
 
@@ -69,7 +69,7 @@ function shakira() {
   if (choice === "beyonce and shakira") {
       imageElement.src = "https://3.bp.blogspot.com/-4lN1cdzcQYA/UO1mmrHmvHI/AAAAAAAAIMg/Zd6LCxJOnWs/s1600/Shakira-Beyonce.jpg";
   } else {
-      imageElement.src = "https://th.bing.com/th/id/OIP.OH8xtUjQwnJBeCero53anQAAAA?rs=1&pid=ImgDetMain";
+      imageElement.src = "https://i.pinimg.com/474x/5a/e9/0e/5ae90e60e56dc3965da6d1d398ecbca0.jpg";
   }
 
 
@@ -98,7 +98,7 @@ function laufey() {
   if (choice === "laufey and beabadoobee") {
       imageElement.src = "https://atwoodmagazine.com/wp-content/uploads/2023/11/beabadoobee-and-Laufey-768x546.jpeg";
   } else {
-      imageElement.src = "https://th.bing.com/th/id/OIP.OH8xtUjQwnJBeCero53anQAAAA?rs=1&pid=ImgDetMain";
+      imageElement.src = "https://i.pinimg.com/474x/5a/e9/0e/5ae90e60e56dc3965da6d1d398ecbca0.jpg";
   }
 
 
@@ -125,7 +125,7 @@ function marina() {
   if (choice === "marina and the diamonds") {
       imageElement.src = "https://th.bing.com/th/id/R.a03cc3b8316464b53ba4fb3672f198a6?rik=k%2bGLHLfAncBqzg&riu=http%3a%2f%2fimages6.fanpop.com%2fimage%2fpolls%2f1349000%2f1349449_1393897237158_full.jpg%3fv%3d1393897240&ehk=t%2bdTxyU3eEK3ZjSGaOFvgf78FDEVfOaox2V%2f56MKjcc%3d&risl=&pid=ImgRaw&r=0";
   } else {
-      imageElement.src = "https://th.bing.com/th/id/OIP.OH8xtUjQwnJBeCero53anQAAAA?rs=1&pid=ImgDetMain";
+      imageElement.src = "https://i.pinimg.com/474x/5a/e9/0e/5ae90e60e56dc3965da6d1d398ecbca0.jpg";
   }
 
 
