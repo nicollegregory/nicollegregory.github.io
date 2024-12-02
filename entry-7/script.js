4e81c613dd2e71bd47bd97373eef808ebddda8b4
nicollegregory <gregn565@newschool.edu>
2024-12-01 22:15:39 -0500

    entry 7 update

--- entry-7/script.js
+++ entry-7/script.js
@@ -1,143 +1,144 @@
 $(window).scroll(function() {
-  $(".space").css("opacity", 1 - $(window).scrollTop() / 250);
-});
-
+    $(".space").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+ 
 function justinb() {
-let choice = document.getElementById("answer").value.trim().toLowerCase();
-let imageElement = document.getElementById("my-image");
+  let choice = document.getElementById("answer").value.trim().toLowerCase();
+  let imageElement = document.getElementById("my-image");
+
+  
+  if (choice === "") {
+      alert("Please enter an answer!");
+      return; 
+  }
 
 
-if (choice === "") {
-    alert("Please enter an answer!");
-    return; 
-}
+  if (choice === "justin bieber") {
+      imageElement.src = "https://th.bing.com/th/id/R.d4eb10cb28a5f707695129a546d3c812?rik=7hOCM4WP3cWbqw&riu=http%3a%2f%2fimages5.fanpop.com%2fimage%2fphotos%2f26700000%2fJustin-Bieber-2011-justin-bieber-26788277-500-500.jpg&ehk=PQUtle8Odc3RUjCoY9pDt87mKaHpPC%2fLODzkZIJGdeU%3d&risl=&pid=ImgRaw&r=0";
+  } else {
+      imageElement.src = "https://i.pinimg.com/originals/5c/da/67/5cda676e0c4a313db0715118063b3084.png";
+  }
 
 
-if (choice === "justin bieber") {
-    imageElement.src = "https://i.pinimg.com/originals/39/d0/38/39d03876a2aa6b1fdebc154f2486f48a.jpg";
-} else {
-    imageElement.src = "https://th.bing.com/th/id/OIP.tn2Fc5Wr7gKDt9rkIE-a4AAAAA?rs=1&pid=ImgDetMain";
-}
-
-
-imageElement.style.display = "block";
+  imageElement.style.display = "block";
 }
 
 function checkEnter(event) {
-if (event.key === "Enter") {
-    justinb();
-}
+  if (event.key === "Enter") {
+      justinb();
+  }
 }
 
 function brown() {
-let choice = document.getElementById("answer2").value.trim().toLowerCase();
-let imageElement = document.getElementById("image");
+  let choice = document.getElementById("answer2").value.trim().toLowerCase();
+  let imageElement = document.getElementById("image");
+
+  
+  if (choice === "") {
+      alert("Please enter an answer!");
+      return; 
+  }
 
 
-if (choice === "") {
-    alert("Please enter an answer!");
-    return; 
-}
+  if (choice === "chris brown") {
+      imageElement.src = "https://wallpaperaccess.com/full/56356.jpg";
+  } else {
+      imageElement.src = "https://i.pinimg.com/originals/5c/da/67/5cda676e0c4a313db0715118063b3084.png";
+  }
 
 
-if (choice === "chris brown") {
-    imageElement.src = "https://th.bing.com/th/id/R.6a1c7507b9b4c08b586315198fdde2c7?rik=wM8zIz87Rv7YKw&pid=ImgRaw&r=0";
-} else {
-    imageElement.src = "https://th.bing.com/th/id/OIP.tn2Fc5Wr7gKDt9rkIE-a4AAAAA?rs=1&pid=ImgDetMain";
-}
-
-
-imageElement.style.display = "block";
-}
-
-function checkEnter(event) {
-if (event.key === "Enter") {
-    brown();
-}
-}
-
-function gambino() {
-let choice = document.getElementById("answer3").value.trim().toLowerCase();
-let imageElement = document.getElementById("teen");
-
-
-if (choice === "") {
-    alert("Please enter an answer!");
-    return; 
-}
-
-
-if (choice === "childish gambino") {
-    imageElement.src = "https://runthetrap.com/wp-content/uploads/2019/04/childish-gambino-1024x682.jpg";
-} else {
-    imageElement.src = "https://th.bing.com/th/id/OIP.tn2Fc5Wr7gKDt9rkIE-a4AAAAA?rs=1&pid=ImgDetMain";
-}
-
-
-imageElement.style.display = "block";
+  imageElement.style.display = "block";
 }
 
 function checkEnter(event) {
-if (event.key === "Enter") {
-    gambino();
+  if (event.key === "Enter") {
+      brown();
+  }
 }
+function jonas() {
+  let choice = document.getElementById("answer3").value.trim().toLowerCase();
+  let imageElement = document.getElementById("teen");
+
+  
+  if (choice === "") {
+      alert("Please enter an answer!");
+      return; 
+  }
+
+
+  if (choice === "jonas brothers") {
+      imageElement.src = "https://img.nbc.com/mpx-static/image/601/50/190511_3954445_Jonas_Brothers__Cool_Burnin__Up__Live_.jpg";
+  } else {
+      imageElement.src = "https://i.pinimg.com/originals/5c/da/67/5cda676e0c4a313db0715118063b3084.png";
+  }
+
+
+  imageElement.style.display = "block";
 }
+
+function checkEnter(event) {
+  if (event.key === "Enter") {
+      jonas();
+  }
+}
+
+
 
 function rhianna() {
-let choice = document.getElementById("answer4").value.trim().toLowerCase();
-let imageElement = document.getElementById("beat");
+  let choice = document.getElementById("answer4").value.trim().toLowerCase();
+  let imageElement = document.getElementById("beat");
+
+  
+  if (choice === "") {
+      alert("Please enter an answer!");
+      return; 
+  }
 
 
-if (choice === "") {
-    alert("Please enter an answer!");
-    return; 
-}
+  if (choice === "rhianna") {
+      imageElement.src = "https://th.bing.com/th/id/OIP.ngxy1gYR5FjwMEZo6KSGDgHaJv?rs=1&pid=ImgDetMain";
+  } else {
+      imageElement.src = "https://i.pinimg.com/originals/5c/da/67/5cda676e0c4a313db0715118063b3084.png";
+  }
 
 
-if (choice === "rhianna") {
-    imageElement.src = "https://th.bing.com/th/id/R.022bed083babd68c00291b97ac11a8d8?rik=9Al0otW5izQuQg&riu=http%3a%2f%2fthatgrapejuice.net%2fwp-content%2fuploads%2f2016%2f04%2frihanna-1-600x397.jpg&ehk=uXX%2fWTUeEDLkFl7V8TKcBAeW4QpUjeg4Zs9wGUeAiMo%3d&risl=&pid=ImgRaw&r=0";
-} else {
-    imageElement.src = "https://th.bing.com/th/id/OIP.tn2Fc5Wr7gKDt9rkIE-a4AAAAA?rs=1&pid=ImgDetMain";
-}
-
-
-imageElement.style.display = "block";
+  imageElement.style.display = "block";
 }
 
 function checkEnter(event) {
-if (event.key === "Enter") {
-    rhianna();
-}
+  if (event.key === "Enter") {
+      rhianna();
+  }
 }
 
 function avril() {
-let choice = document.getElementById("answer5").value.trim().toLowerCase();
-let imageElement = document.getElementById("moon");
+  let choice = document.getElementById("answer5").value.trim().toLowerCase();
+  let imageElement = document.getElementById("moon");
+
+  
+  if (choice === "") {
+      alert("Please enter an answer!");
+      return; 
+  }
 
 
-if (choice === "") {
-    alert("Please enter an answer!");
-    return; 
-}
+  if (choice === "avril lavigne") {
+      imageElement.src = "https://i.pinimg.com/736x/49/43/c2/4943c201af4a0278ade403a5c610466b.jpg";
+  } else {
+      imageElement.src = "https://i.pinimg.com/originals/5c/da/67/5cda676e0c4a313db0715118063b3084.png";
+  }
 
 
-if (choice === "avril lavigne") {
-    imageElement.src = "https://th.bing.com/th/id/R.e58ec37194e8b319262531e2e15ffe31?rik=ONrwiI%2fe0imDpA&riu=http%3a%2f%2fimages2.fanpop.com%2fimages%2fphotos%2f4200000%2fAvril-Lavigne-avril-lavigne-4213329-2071-2560.jpg&ehk=D%2fZ4mjxiYNE5MHK%2fs7LEbFXmbEckMa2TfvV1YEY1sbk%3d&risl=&pid=ImgRaw&r=0";
-} else {
-    imageElement.src = "https://th.bing.com/th/id/OIP.tn2Fc5Wr7gKDt9rkIE-a4AAAAA?rs=1&pid=ImgDetMain";
-}
-
-
-imageElement.style.display = "block";
+  imageElement.style.display = "block";
 }
 
 function checkEnter(event) {
-if (event.key === "Enter") {
-    avril();
-}
+  if (event.key === "Enter") {
+      avril();
+  }
 }
 
 
- window.onload = function() {
-      var audio = document.getElementById('myAudio');
-      audio.play();}  
+   window.onload = function() {
+        var audio = document.getElementById('myAudio');
+        audio.play();}  
